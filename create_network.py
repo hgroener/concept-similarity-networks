a149e3cd263cb1bf8cd2fce51822a5a14ba3b300
hgroener <hannes.groener@uni-bielefeld.de>
2021-01-10 14:59:50 +0100

    - made it so that only concepts that are in CLICS are mapped by mapping.py - changed file structure - create_network.py now produces a .gmv-file containing the network based on the CLICS-concepts and word2vec similarities.

---
+++ create_network.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import gensim
+import csv
+import pandas as pd
+import networkx as nx
+import itertools
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import random as rd
+from pyconcepticon import Concepticon
+
+
+# In[4]:
+
+
+def read_mappings(table):
+    with open(table) as fd:
+        fd.readline()
+        rd = csv.reader(fd, delimiter="\t", quotechar='"')
+        mapped_vocab = []
+        for a, b, c, d, e in rd:
+            mapped_vocab.append(a)
+    return mapped_vocab
+
+
+# In[5]:
+
+
+def create_edges(model, vocab, threshold, filename):
+    brown_model = gensim.models.Word2Vec.load(model)
+    edges = []
+    for word, other_word in tqdm(itertools.combinations(vocab, 2)):
+        similarity = brown_model.wv.similarity(word, other_word)
+        if similarity > threshold:
+            edges.append((word, other_word, str(similarity)))
+    print("edges per node:" + str(len(edges)/len(vocab)))
+    with open(filename, 'w') as f:
+        for edge in edges:
+            f.write("%s\n" % str(edge))
+    return edges
+
+
+# In[6]:
+
+
+def create_network(vocab, weighted_edges):
+    word2vec_graph = nx.Graph()
+    word2vec_graph.add_nodes_from(vocab)
+    word2vec_graph.add_weighted_edges_from(weighted_edges)
+    return word2vec_graph
+
+
+# In[7]:
+
+
+if __name__ == "__main__":
+    mapped_vocab = read_mappings("output/shared_concepts.tsv")
+    edges = create_edges("output/brown_model.model", mapped_vocab, 0.97, "output/edges.txt")
+    word2vec_graph = create_network(mapped_vocab, edges)
+    nx.write_gml(word2vec_graph, "output/word2vec_graph.gmv")
+
+
+# In[ ]:
+
+
+
+
--- mapping.py
+++ mapping.py
@@ -1,17 +1,31 @@
 from pysen.glosses import to_concepticon
+from pyconcepticon import Concepticon
 
-table = []
-with open('model_vocab.txt') as data:
-    for word in map(lambda x: x.strip(), data):
+con = Concepticon()
+clics_data = []
+for concept in con.conceptlists['Rzymski-2020-1624'].concepts.values():
+    clics_data += [[concept.concepticon_id, concept.concepticon_gloss, concept.attributes['community'], concept.attributes['weighted_family_degree'], concept.attributes['weighted_language_degree']]]    
+
+mapped_concepts = []
+with open('output/model_vocab.txt') as vocab:
+    for word in map(lambda x: x.strip(), vocab):
         mapped = to_concepticon([{'gloss': word}])
         for res in mapped[word]:
-            table += [[word]+res]
-with open('mapped.tsv', 'w') as f:
+            mapped_concepts += [[word]+res]    
+
+shared_concepts = []
+clics_IDs = [ID for [ID, gloss, community, family, language] in clics_data]
+for concept in mapped_concepts:
+    concepticon_id = concept[1]
+    if concepticon_id in clics_IDs:
+        shared_concepts.append(concept)
+
+with open('output/shared_concepts.tsv', 'w') as f:
     f.write('\t'.join(
         [
             'Word', 'Concepticon_ID', 'Concepticon_Gloss', 
             'POS', 'Similarity'])+'\n')
-    for line in table:
+    for line in shared_concepts:
         f.write('\t'.join([str(x) for x in line])+'\n')
 
-print('[i] found {0} matches'.format(len(table)))
+print('[i] found {0} matches'.format(len(shared_concepts)))
