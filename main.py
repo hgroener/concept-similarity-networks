b2f5110eed7ee67883ae0630efe8d75fa2cea68c
hgroener <hannes.groener@uni-bielefeld.de>
2021-03-23 20:49:59 +0100

    changed file structure and added evaluation.py for getting all results at once.

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import word2vecmodel
 import mapping
 import create_network
 import generate_subgraph
-import pairwise_evaluation
-import get_b_cubed
-import get_assortativity
-import get_adj_rand
+import evaluation
 
 corpus_path = 'Brown/brown.csv'
 model_type = 'Brown Model'
@@ -17,15 +14,11 @@ threshold = 0.98
 w2v_gml_path = 'output/word2vec_graph.gml'
 clics_gml_path = 'output/network-3-families.gml'
 subgraph_output_path = 'output/clics_subgraph.gml'
-result_path_pe = "output/pairwise_evaluation.txt"
-result_path_b2 = "output/b_cubed.txt"
+result_path = "output/evaluation.txt"
 
 if __name__=="__main__":
     word2vecmodel.build_model(corpus_path, model_path, vocab_path)
     mapping.get_shared_concepts(vocab_path, shared_concepts_path)
     create_network.get_gml(shared_concepts_path, model_path, edges_path, threshold, w2v_gml_path)
     generate_subgraph.get_subgraph(w2v_gml_path, clics_gml_path, subgraph_output_path)
-    pairwise_evaluation.pairwise_evaluation(subgraph_output_path, w2v_gml_path, model_type, result_path_pe)
-    get_b_cubed.get_b_cubed(w2v_gml_path, subgraph_output_path, model_type, result_path_b2, threshold)
-    get_assortativity.get_assortativity(w2v_gml_path, subgraph_output_path)
-    print(get_adj_rand.get_adj_rand(w2v_gml_path, subgraph_output_path))
+    evaluation.evaluate(w2v_gml_path, subgraph_output_path, model_type, threshold, result_path)
